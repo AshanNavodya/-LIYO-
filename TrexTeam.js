1a2793ed544aae5d6cd7244d3182c6f16bff989f
AshanNavodya <106447143+AshanNavodya@users.noreply.github.com>
2022-05-29 21:36:32 +0530

    Create TrexTeam.js

---
+++ TrexTeam.js
@@ -0,0 +1,5 @@
+module.exports = {
+    NL_OWNER: '94773109531', // Trex Team - NL Owner
+    TREX_TEAM: '94773109531', // Team - Trex TEAM
+    TREX_OWNER: '94773109531' // Naodya Liyanage - Trex Owner --> For The Friendship And co Owner in NL 
+};
